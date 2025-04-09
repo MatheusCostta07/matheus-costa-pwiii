ee81fa8c761227b9458cbbbe8a8c600e9e9f865e
Matheus Nascimento Costa <132940705+MatheusCostta07@users.noreply.github.com>
2025-04-09 20:13:18 -0300

    laco-repeticao.php

--- aula1/laco-repeticao.php
+++ aula1/laco-repeticao.php
@@ -1,25 +1,183 @@
-<form method="POST">
-    <label>Valor 1:</label>
-    <input type="number" name="n1" step="any" required><br>
-    <label>Valor 2 (≠ 0):</label>
-    <input type="number" name="n2" step="any" required><br>
-    <button type="submit">Dividir</button>
-</form>
-
 <?php
 
+echo "Questão 1 até a 4";
+echo "<br><br>";
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $n1 = floatval($_POST['n1']);
-    $n2 = floatval($_POST['n2']);
+define("VALOR_INVALIDO", "VALOR INVÁLIDO");
 
-    if ($n2 == 0) {
-        echo "<p style='color:red;'>VALOR INVÁLIDO: divisão por zero!</p>";
-    } else {
-        $res = $n1 / $n2;
-        echo "<h3>Resultado: $res</h3>";
+do {
+    echo "Digite o primeiro valor: ";
+    $valor1 = floatval(fgets(STDIN));
+
+    echo "Digite o segundo valor: ";
+    $valor2 = floatval(fgets(STDIN));
+
+    if ($valor2 == 0) {
+        echo VALOR_INVALIDO . ". O segundo valor não pode ser zero.\n";
     }
+} while ($valor2 == 0);
+
+
+$resultado = $valor1 / $valor2;
+echo "Resultado da divisão: " . $resultado . "\n";
+echo "<br><br><br>";
+
+
+//------------------------------------------------------------------------------------------------------------------------
+
+
+echo "Questão 5";
+echo "<br><br>";
+
+do {
+    echo "Digite a nota da primiera avaliação: ";
+    $nota_um = floatval(fgets(STDIN));
+} while ($nota_um < 0 || $nota_um > 10);
+
+
+do {
+    echo "Digite a nota da prova avaliação: ";
+    $nota_dois = floatval(fgets(STDIN));
+} while ($nota_dois < 0 || $nota_dois > 10);
+
+
+$media_final = ($nota_um + $nota_dois) / 2;
+
+
+echo "A média do aluno é: " . number_format($media_final, 2) . "\n"; 
+echo "<br><br><br>";
+
+
+//------------------------------------------------------------------------------------------------------------------------
+
+
+echo "Questão 6";
+echo "<br><br>";
+
+do {
+    echo "Digite a nota da primiera avaliação: ";
+    $nota_um = floatval(fgets(STDIN));
+} while ($nota_um < 0 || $nota_um > 10);
+
+
+do {
+    echo "Digite a nota da prova avaliação: ";
+    $nota_dois = floatval(fgets(STDIN));
+} while ($nota_dois < 0 || $nota_dois > 10);
+
+
+$media_final = ($nota_um + $nota_dois) / 2;
+
+
+echo "A média do aluno é: " . number_format($media_final, 2) . "\n";
+
+
+echo "Deseja fazer um novo cálculo (S/N)? ";
+    $resposta = strtoupper(trim(fgets(STDIN)));
+    while ($resposta == 'S');
+
+echo "Programa encerrado.\n";
+echo "<br><br><br>";
+
+
+//------------------------------------------------------------------------------------------------------------------------
+
+
+echo "Questão 7";
+echo "<br><br>";
+
+for ($i = 1; $i <= 10; $i++) {
+    echo $i . "\n";
+}
+echo "<br><br><br>";
+
+
+//------------------------------------------------------------------------------------------------------------------------
+
+
+echo "Questão 8";
+echo "<br><br>";
+
+for ($i = 10; $i >= 1; $i--) {
+    echo $i . "\n";
+}
+echo "<br><br><br>";
+
+
+//------------------------------------------------------------------------------------------------------------------------
+
+
+echo "Questão 9";
+echo "<br><br>";
+
+$num = 101;
+
+for ($i = 0; $i < 10; $i++) {
+    echo $num . "\n";  
+    $num++;           
+}
+echo "<br><br><br>";
+
+
+//------------------------------------------------------------------------------------------------------------------------
+
+
+echo "Questão 10";
+echo "<br><br>";
+
+echo "Digite um valor: ";
+$valor = (int)fgets(STDIN);  
+
+for ($i = 1; $i <= $valor; $i++) {
+    echo $i . "\n"; 
+}
+echo "<br><br><br>";
+
+
+//------------------------------------------------------------------------------------------------------------------------
+
+
+echo "Questão 11";
+echo "<br><br>";
+
+do {
+    echo "Digite um valor: ";
+    $valor = (int)fgets(STDIN);  
+} 
+while ($valor <= 0);  
+
+for ($i = 1; $i <= $valor; $i++) {
+    echo $i . "\n"; 
 }
 
-  
+echo "<br><br><br>";
+
+
+//------------------------------------------------------------------------------------------------------------------------
+
+
+echo "Questão 12";
+echo "<br><br>";
+
+echo "Vamos conhecer a tabuada do 8:\n";
+for ($i = 1; $i <= 10; $i++) {
+    echo "8 x $i = " . (8 * $i) . "\n";
+}
+echo "<br><br><br>";
+
+
+//------------------------------------------------------------------------------------------------------------------------
+
+
+echo "Questão 13";
+echo "<br><br>";
+
+do {
+    $valor_tabuada = (int)readline("Digite um valor entre 1 e 10: ");
+} while ($valor_tabuada < 1 || $valor_tabuada > 10);
+
+echo "Tabuada do $valor_tabuada: \n";
+for ($i = 1; $i <= 10; $i++) {
+    echo "$valor_tabuada x $i = " . ($valor_tabuada * $i) . "\n";
+}
 ?>
