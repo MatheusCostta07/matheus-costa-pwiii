4ab5fdd8b33763d4f21ac260e431f481196861d8
Matheus Nascimento Costa <132940705+MatheusCostta07@users.noreply.github.com>
2025-04-09 19:56:57 -0300

    laco-repeticao.php

--- aula1/laco-repeticao.php
+++ aula1/laco-repeticao.php
@@ -1,1 +1,25 @@
+<form method="POST">
+    <label>Valor 1:</label>
+    <input type="number" name="n1" step="any" required><br>
+    <label>Valor 2 (≠ 0):</label>
+    <input type="number" name="n2" step="any" required><br>
+    <button type="submit">Dividir</button>
+</form>
 
+<?php
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $n1 = floatval($_POST['n1']);
+    $n2 = floatval($_POST['n2']);
+
+    if ($n2 == 0) {
+        echo "<p style='color:red;'>VALOR INVÁLIDO: divisão por zero!</p>";
+    } else {
+        $res = $n1 / $n2;
+        echo "<h3>Resultado: $res</h3>";
+    }
+}
+
+  
+?>
